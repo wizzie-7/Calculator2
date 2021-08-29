68d19d3090d9fd44ee1a448682385edc1ce22bb4
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 18:52:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Calcy;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+
+            Addition ad = new Addition();
+            Substraction sub = new Substraction();
+            Multiplication mul = new Multiplication();
+            Division div = new Division();
+            Qual eq = new Qual();
+            Greater gr = new Greater();
+
+            
+            char conf;
+
+            do
+            {
+                Console.WriteLine("Press specific key to perform opertion from following operations");
+                Console.WriteLine("Addition = 1");
+                Console.WriteLine("Substraction = 2");
+                Console.WriteLine("Multiplication = 3");
+                Console.WriteLine("Devision = 4");
+                Console.WriteLine("Equal = 5");
+                Console.WriteLine("Greater = 6");
+
+                int Var = Convert.ToInt32(Console.ReadLine());
+                
+                switch (Var)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            ad.Add(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            sub.subs(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            mul.mult(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            div.Divi(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            eq.eqa(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Enter numbers1  :");
+                            a = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter numbers2  :");
+                            b = Convert.ToDouble(Console.ReadLine());
+                            gr.great(a, b);
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Select correct option, Thank you!!"); 
+                            Console.WriteLine("For continue press Y for cancle press N");
+                            conf = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                }
+            } while(conf == 'Y');
+            {
+                Console.WriteLine("Thank You!!!!");
+            }
+           }
+    }
+}
